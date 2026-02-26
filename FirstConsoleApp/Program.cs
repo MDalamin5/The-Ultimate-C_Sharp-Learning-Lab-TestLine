9911c55d4d092d0aed9c880a3bc2524277d783ac
mdalamin5 <mdal.amin5@northsouth.edu>
2026-02-26 14:38:09 +0600

    multi-class accessing

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System.Reflection.Metadata;
 
+// class Test
+// {
+//     void display(string name)
+//     {
+//         Console.WriteLine($"ITs call from other functions...{name}");
+//         for(int i=0; i<5; i++)
+//         {
+//             // Console.WriteLine(i);
+//         }
+//     }
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("test line");
+//         Test obj = new Test();
+//         obj.display("hello");
+//         obj.display("hi");
+//     }
+// }
 
-class Test
+class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("test line");
+        Dog myDog = new Dog("Tommy");
+        myDog.Bark();
     }
 }
---
+++ FirstConsoleApp/dog.cs
@@ -0,0 +1,14 @@
+public class Dog
+{
+    public string Name { get; set; }
+
+    public Dog(string name)
+    {
+        Name = name;
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine($"{Name} is barking!");
+    }
+}
