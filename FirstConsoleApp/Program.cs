b4f32ebf5a03db2add1e2117d8916c2b56d47e30
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-06 11:45:10 +0600

    merge other class with the program.cs file

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,82 +1,23 @@
-﻿using System.Net.Security;
-using System.Reflection.Metadata;
-using System.Security.Cryptography.X509Certificates;
+﻿using FirstConsoleApp.Basics;
 
 class Test
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Assalamualikum...");
-        /*
-            there are different type of variable
-            int, string, bool, double, object type
-
-        */
-        string userName = "Md Al Amin";
-        char helloChar = 'c';
-        Console.WriteLine(userName);
-        Console.WriteLine(12.5);
-        Console.WriteLine($"Try to print the char {helloChar}");
-
-        // bool data type
-        bool isDelete = true;
-        if (isDelete)
-        {
-            Console.WriteLine($"Is-Delete is True:  {userName}");
-        }
-        else
-            Console.WriteLine("isDelete is False...");
-
-        Test ownObj = new Test();
-
-        ownObj.demoFunction(userName);
-
-        // Multiple variable
-        int num1, num2, num3;
-        num1=num2=num3=10;
-
-        Console.WriteLine($"The number is {num1}, {num2}, {num3}");
-
-
-        // datatype conversations use Convert functions
-        int salary = 4000;
-        Console.WriteLine(Convert.ToString(salary));
-        Console.WriteLine(Convert.ToString(salary).GetType());
-
-        //data-conversations
-        string data = "10";
-        bool ssuccess = int.TryParse(data, out int result);
-        Console.WriteLine($"This string value is: {data}, and convert success: {ssuccess}, and th result is: {result}");
-
-        // take input from user
-        Console.Write("Can you please enter your name: ");
-        string? inputName = Console.ReadLine();
-        // Console.WriteLine($"Your name is: {inputName}");
-
-        // take input integer data
-        int personAge;
-        Console.Write("Enter your age: ");
-        personAge = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Got your data...");
-        Console.WriteLine($"Your name is: {inputName}, Age: {personAge}");
-
-        // take double input
-        double inputDouble;
-        Console.WriteLine("Enter a Double number: ");
-
-        inputDouble = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine($"Your name is: {inputName}, Age: {personAge}, your GPA: {inputDouble}");
-
-
-
-
-
-    }
-
-    void demoFunction(string Uname)
-    {
         
-        Console.WriteLine($"The name is: {Uname}");
+        Console.WriteLine("Starting Practice...");
+
+        // Simply uncomment the one you want to run today:
+        
+        SuperBasic.Basic();
+        // Loops.Run();
+        // Arrays.Run();
+        // Variables.Run();
+
+        Console.WriteLine("Done.");
+
+
     }
+
 }
 
