55ddb63e52bd31cd5f4c1db9355715ccea1f9700
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-08 14:56:46 +0600

    just typecheking in c#

--- FirstConsoleApp/Basics/inputValidationLoopTermination.cs
+++ FirstConsoleApp/Basics/inputValidationLoopTermination.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Metadata;
 namespace FirstConsoleApp.Basics
 {
     public class LoopTermination
@@ -10,13 +11,29 @@ namespace FirstConsoleApp.Basics
                 Console.Write("Enter the number which is 1 to 10: ");
 
                 string input = Console.ReadLine() ?? "";
-                input = input.ToLower();
+                input = input.ToLower().Trim();
                 if (input == "exit")
                 {
                     Console.WriteLine("You press Exit so Good Bye...");
                     break;
                 }
+
+                if (!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    continue;
+                }
+
+                if (!(number >= 0 && number <= 5))
+                {
+                    Console.WriteLine("The number is not in the range..");
+                    continue;
+                }
+                 int add = number + number;
+                    Console.WriteLine($"Sum is: {add}");
+
             }
+            
 
             
         }
