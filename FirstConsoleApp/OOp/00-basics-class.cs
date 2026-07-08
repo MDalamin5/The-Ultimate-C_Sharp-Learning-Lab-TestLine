a66ca3e5a93cc75757ddb698203da4723857a066
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-08 15:28:37 +0600

    update the tostring method

--- FirstConsoleApp/OOp/00-basics-class.cs
+++ FirstConsoleApp/OOp/00-basics-class.cs
@@ -3,8 +3,13 @@ namespace FirstConsoleApp.OOp
 {
     class DemoPerson
     {
-        public string name;
+        public string? name;
         public int age;
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}";
+        }
     }
 
     public class ClassTEst{
@@ -17,8 +22,10 @@ namespace FirstConsoleApp.OOp
             DemoPerson p1 = new DemoPerson();
             p1.name = "Md Al Amin";
             p1.age = 32;
+            DemoPerson p2 = new DemoPerson {name = "Miad", age = 33};
 
             Console.WriteLine($"Name: {p1.name}, Age: {p1.age}");
+            Console.WriteLine(p2);
         }
     
     }
