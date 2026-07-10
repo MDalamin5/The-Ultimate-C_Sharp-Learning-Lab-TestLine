19fe2ec533c1301e3b6abac48217eefbeed20f1b
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-10 22:41:35 +0600

    init the variable and constractor only

--- FirstConsoleApp/OOp/student-manage-project.cs
+++ FirstConsoleApp/OOp/student-manage-project.cs
@@ -1,4 +1,18 @@
+using System.Globalization;
+
 namespace ManagementProject
 {
-    
+    public class Student
+    {
+        public string name {get; set;} = "";
+        public string dateOfBirth {get; set;} = "";
+        public string rollNumber {get; set;} = "";
+
+        public Student(string name, string dateOfBirth, string rollNumber)
+        {
+            this.name = name;
+            this.dateOfBirth = dateOfBirth;
+            this.rollNumber = rollNumber;
+        }
+    }
 }
