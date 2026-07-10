120b5a41ce9f3bf29224daa6e4376aa683fae230
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-10 22:56:25 +0600

    private variable set vai try catch

--- FirstConsoleApp/OOp/student-manage-project.cs
+++ FirstConsoleApp/OOp/student-manage-project.cs
@@ -5,13 +5,27 @@ namespace ManagementProject
     public class Student
     {
         public string name {get; set;} = "";
-        public string dateOfBirth {get; set;} = "";
+        private DateTime _dateOfBirth;
         public string rollNumber {get; set;} = "";
 
-        public Student(string name, string dateOfBirth, string rollNumber)
+
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Date can not be future pagla...");
+                }
+                _dateOfBirth = value;
+            }
+
+        }
+        public Student(string name, DateTime dateOfBirth, string rollNumber)
         {
             this.name = name;
-            this.dateOfBirth = dateOfBirth;
+            DateOfBirth = dateOfBirth;
             this.rollNumber = rollNumber;
         }
     }
