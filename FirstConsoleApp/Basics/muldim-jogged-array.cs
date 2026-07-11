4875117daf4cdf795397d9d495f0986b09f9c857
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-11 15:32:07 +0600

    take array input fromt the user

--- FirstConsoleApp/Basics/muldim-jogged-array.cs
+++ FirstConsoleApp/Basics/muldim-jogged-array.cs
@@ -40,10 +40,25 @@ public class advArray
         jogged[2] = new int[2];
         jogged[3] = new int[4];
 
-        jogged[0] = [1, 2, 4];
-        jogged[1] = [2];
-        jogged[2] = [2, 4];
-        jogged[3] = [5, 6, 7, 7];
+        //array input take from user.
+        for(int i=0; i<jogged.Length; i++)
+        {
+            for(int j=0; j<jogged[i].Length; j++)
+            {
+                Console.Write($"Please Enter the value of: Jogged[{i}][{j}]: ");
+                
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                jogged[i][j] = userInput;
+            }
+        }
+
+        // jogged[0] = [1, 2, 4];
+        // jogged[1] = [2];
+        // jogged[2] = [2, 4];
+        // jogged[3] = [5, 6, 7, 7];
+
+        
+
 
         for(int i=0; i<jogged.Length; i++)
         {
