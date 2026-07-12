f1400725918c7c35297a2613518abbc7eeddc492
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-12 11:52:08 +0600

    basics list with linq

--- FirstConsoleApp/OOp/generic.cs
+++ FirstConsoleApp/OOp/generic.cs
@@ -5,7 +5,37 @@ namespace GenericCsharp
     {
         public static void Run()
         {
-            Console.WriteLine("HEllo...");
+            List<string> names = ["Alamin", "miad"];
+
+            // add more name
+            names.Add("Nirob");
+            names.Add("Muzahid");
+
+            // adding multiple item in once
+            string[] newStudents = ["Sakib", "Tamim"];
+            names.AddRange(newStudents);
+
+            foreach(string name in names)
+            {
+                Console.Write($"{name}, ");
+                
+            }
+
+            Console.WriteLine();
+
+            // remove item from the list
+            names.Remove("Nirob");
+            names.RemoveAt(0);
+
+            Console.WriteLine("After removing");
+
+            foreach(string name in names)
+            {
+                Console.Write($"{name}, ");
+                
+            }
+            
+            
             
         }
     }
