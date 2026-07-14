c4f96216309532fda125188d26fbb08428823c06
mdalamin5 <mdal.amin5@northsouth.edu>
2026-07-14 22:44:30 +0600

    program class defined

--- MasteringOOP/Program.cs
+++ MasteringOOP/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using MasteringOOP.Inheritance;
+
+namespace MasteringOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Inheritance Testing ---");
+
+            // We instantiate the Child class.
+            // It automatically runs the Parent's constructor first!
+            CustomerEntity customer = new CustomerEntity(
+                fullName: "John Doe", 
+                email: "john.doe@example.com", 
+                createdBy: "SystemAdmin"
+            );
+
+            Console.WriteLine(customer.ToString());
+        }
+    }
+}
