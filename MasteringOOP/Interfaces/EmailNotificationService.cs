15347c7ec9224c294ac01dbe284eef345f4560b3
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-17 09:49:15 +0600

    email notifications services added

--- MasteringOOP/Interfaces/EmailNotificationService.cs
+++ MasteringOOP/Interfaces/EmailNotificationService.cs
@@ -1,1 +1,12 @@
 using System;
+
+namespace MasteringOOP.Interface;
+
+public class EmailNotificationService : INotificationService
+{
+    public void SendReceipt(string customerEmail, decimal amount)
+    {
+        Console.WriteLine($"Email send Success: {customerEmail} with amount: {amount}");
+        
+    }
+}
