c87718f8883e4575156f5ebe0c33318674f2a30f
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-20 09:23:49 +0600

    digitalProduct class implemented

--- MasteringOOP/MiniProject/DigitalProduct.cs
+++ MasteringOOP/MiniProject/DigitalProduct.cs
@@ -1,3 +1,17 @@
 using System;
 namespace MasteringOOP.ShoppingCartProject;
 
+public class DigitalProduct: Product
+{
+    public string DownloadLink {get; private set;}
+
+    public DigitalProduct(string downloadLink, string ProductName, decimal ProductPrice) : base(ProductName, ProductPrice)
+    {
+        DownloadLink = downloadLink;
+    }
+
+    public override void DisplayProductInfo()
+    {
+        Console.WriteLine($"Product Name: {Name}, Product Price: {Price}, DownloadLink: {DownloadLink}");
+    }
+}
