29d2bd4cb53412e2b463ed1c67413bec1e78fbb1
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-20 12:37:58 +0600

    explore most vanilla dotnet api endpoint building

--- EcommerceWebApi/Program.cs
+++ EcommerceWebApi/Program.cs
@@ -2,6 +2,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+
+// REST API: put, post, put, delete
+
+app.MapGet("/hello", () =>
+{
+    return "Its a First messages From the Dotnet backend";
+});
+
+app.MapGet("/", () =>
+{
+    return "Welcome to First .Net api Playground.";
+});
+
+app.MapGet("/demo", () =>
+{
+    return "This is a demo api call.";
+});
+
+app.MapGet("/femo", () =>
+{
+    return "This is a Femo api rrr call.";
+});
 
 app.Run();
