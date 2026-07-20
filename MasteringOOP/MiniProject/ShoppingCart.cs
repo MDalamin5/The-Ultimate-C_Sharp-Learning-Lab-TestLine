469241958cbb998781f5174918ba2a8c53753673
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-20 09:46:36 +0600

    product added with the dependency ingestions

--- MasteringOOP/MiniProject/ShoppingCart.cs
+++ MasteringOOP/MiniProject/ShoppingCart.cs
@@ -1,3 +1,28 @@
 using System;
+using System.Data;
 namespace MasteringOOP.ShoppingCartProject;
 
+public class ShoppingCart
+{
+    private readonly ITaxService _taxService;
+
+    private List<Product> _items = new List<Product>();
+    public ShoppingCart(ITaxService taxService)
+    {
+        _taxService = taxService;
+        // _items.Add(item);
+    }
+
+    public void AddItem(Product product)
+    {
+        if(product == null)
+        {
+            throw new NoNullAllowedException("You can no call the function without product.");
+        }
+
+        _items.Add(product);
+
+
+
+    }
+}
