f7bb65a779c4d388d008b9f8aa72a0e87deaa3c2
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-22 13:04:09 +0600

    start to building the api controller

---
+++ EcommerceWebApi/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebApi.Controllers
+{   
+    [ApiController]
+    [Route("/api/v1/categories/")]
+    public class CategoryController: ControllerBase
+    {
+        
+    }
+}
