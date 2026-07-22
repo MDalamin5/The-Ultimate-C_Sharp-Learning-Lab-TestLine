87bca40987e8bb7cb93454891f4893cc097aa4c3
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-22 20:35:46 +0600

    build my first api in controller file

--- EcommerceWebApi/Controllers/CategoryController.cs
+++ EcommerceWebApi/Controllers/CategoryController.cs
@@ -10,6 +10,19 @@ namespace EcommerceWebApi.Controllers
     [Route("/api/v1/categories/")]
     public class CategoryController: ControllerBase
     {
-        
+        private static List<Category> categories = new List<Category>();
+        // TO read teh category => api/v1/categories
+        [HttpGet]
+        public IActionResult GetCategories([FromQuery] string searchValue = "")
+        {
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                var searchCategories = categories.Where(c => c.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+
+                return Ok(searchCategories);
+            }
+            else
+                return Ok(categories);
+        }
     }
 }
