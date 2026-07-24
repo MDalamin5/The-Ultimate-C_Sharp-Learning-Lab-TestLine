077186a88cb22455522140b397f6f286af07df34
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-24 10:59:26 +0600

    null value warning fixed

--- EcommerceWebApi/DTOs/CategoryCreateDto.cs
+++ EcommerceWebApi/DTOs/CategoryCreateDto.cs
@@ -7,7 +7,7 @@ namespace EcommerceWebApi.DTOs
 {
     public class CategoryCreateDto
     {
-        public string Name {get; set;}
+        public string? Name {get; set;}
         public string Description {get; set;} = string.Empty;
     }
 }
