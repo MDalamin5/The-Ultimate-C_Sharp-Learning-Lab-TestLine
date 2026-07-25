084c8855df0cdd563cab93358a07973f42e8a3bb
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-25 17:34:06 +0600

    Validatoins on DTOs

--- EcommerceWebApi/DTOs/CategoryUpdateDto.cs
+++ EcommerceWebApi/DTOs/CategoryUpdateDto.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcommerceWebApi.DTOs
 {
     public class CategoryUpdateDto
     {
+       
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Category Name At-list 3 Char.")]
         public string? Name {get; set;}
+        [StringLength(300, MinimumLength = 30, ErrorMessage = "Descriptions is More than 30 char.")]
         public string Description {get; set;} = string.Empty;
     }
 }
