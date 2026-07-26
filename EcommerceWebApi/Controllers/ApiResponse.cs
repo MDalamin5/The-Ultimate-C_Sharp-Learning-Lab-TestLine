75ae65916c7abe506e5a080667127ef4a6974b7f
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-26 12:42:36 +0600

    centralized api response construstoctor build

--- EcommerceWebApi/Controllers/ApiResponse.cs
+++ EcommerceWebApi/Controllers/ApiResponse.cs
@@ -7,6 +7,12 @@ namespace EcommerceWebApi.Controllers
 {
     public class ApiResponse<T>
     {
-        
+        public bool Success {get; set;}
+        public string? Messages {get; set;}
+        public T? Data {get; set;}
+        public List<string>? Errors {get; set;}
+
+        public int StatusCode {get; set;}
+        public DateTime TimeStamp {get; set;}
     }
 }
