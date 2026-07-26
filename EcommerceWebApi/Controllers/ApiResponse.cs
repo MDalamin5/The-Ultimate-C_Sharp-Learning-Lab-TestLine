a5c6812ea4879fa7027eb57785cf67957bdec3c5
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-26 12:47:13 +0600

    build api successful response constructor

--- EcommerceWebApi/Controllers/ApiResponse.cs
+++ EcommerceWebApi/Controllers/ApiResponse.cs
@@ -8,11 +8,26 @@ namespace EcommerceWebApi.Controllers
     public class ApiResponse<T>
     {
         public bool Success {get; set;}
-        public string? Messages {get; set;}
+        public string? Message {get; set;}
         public T? Data {get; set;}
         public List<string>? Errors {get; set;}
 
         public int StatusCode {get; set;}
         public DateTime TimeStamp {get; set;}
+
+        // Constructor for Successful Response
+
+        public ApiResponse(T data, int statusCode, string message = "")
+        {
+            Success = true;
+            Message = message;
+            Data = data;
+            Errors = null;
+            StatusCode = statusCode;
+            TimeStamp = DateTime.UtcNow;
+        }
     }
+
+    
+    
 }
