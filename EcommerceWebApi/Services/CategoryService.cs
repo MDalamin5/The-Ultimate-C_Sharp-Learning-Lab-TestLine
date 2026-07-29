4a5f88d7634c7fa02caf6f52d180b19440c72111
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-29 14:56:52 +0600

    getCategories api mapping via Mapper

--- EcommerceWebApi/Services/CategoryService.cs
+++ EcommerceWebApi/Services/CategoryService.cs
@@ -2,26 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using EcommerceWebApi.DTOs;
 using EcommerceWebApi.Interfaces;
 using EcommerceWebApi.Models;
+using EcommerceWebApi.Profies;
 
 namespace EcommerceWebApi.Services
 {
     public class CategoryService: ICategoryService
     {
         private static readonly List<Category> _categories = new List<Category>();
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
 
 
         public List<CategoryReadDto> GetAllCategories()
         {
-            return _categories.Select(c => new CategoryReadDto
-            {
-                CategoryId = c.CategoryId,
-                Name = c.Name,
-                Description = c.Description,
-                CreatedAt = c.CreatedAt
-            }).ToList();
+            // Before Mapping.
+            // return _categories.Select(c => new CategoryReadDto
+            // {
+            //     CategoryId = c.CategoryId,
+            //     Name = c.Name,
+            //     Description = c.Description,
+            //     CreatedAt = c.CreatedAt
+            // }).ToList();
+
+            //After Mapping. All category data map to CategoryReadDto and return.
+
+            return _mapper.Map<List<CategoryReadDto>>(_categories);
         }
 
         public CategoryReadDto? GetCategoryById(Guid categoryId)
@@ -31,14 +44,16 @@ namespace EcommerceWebApi.Services
                 return null;
             else
             {
-                var FoundCategory = new CategoryReadDto
-                {
-                    CategoryId = foundCategory.CategoryId,
-                    Name = foundCategory.Name,
-                    Description = foundCategory.Description,
-                    CreatedAt = foundCategory.CreatedAt
-                };
-                return FoundCategory;
+                // var FoundCategory = new CategoryReadDto
+                // {
+                //     CategoryId = foundCategory.CategoryId,
+                //     Name = foundCategory.Name,
+                //     Description = foundCategory.Description,
+                //     CreatedAt = foundCategory.CreatedAt
+                // };
+                // return FoundCategory;
+
+                return _mapper.Map<CategoryReadDto>(_categories);
             }
         }
 
