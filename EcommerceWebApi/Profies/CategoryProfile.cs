6dab85db3fddde9eaf022061a841e48bafe929c9
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-29 14:46:12 +0600

    profile map building

--- EcommerceWebApi/Profies/CategoryProfile.cs
+++ EcommerceWebApi/Profies/CategoryProfile.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using EcommerceWebApi.Models;
+using EcommerceWebApi.DTOs;
 
 namespace EcommerceWebApi.Profies
 {
-    public class CategoryProfile
+    public class CategoryProfile: Profile
     {
-        
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryReadDto>();
+            CreateMap<CategoryCreateDto, Category>();
+            CreateMap<CategoryUpdateDto, Category>();
+        }
     }
 }
