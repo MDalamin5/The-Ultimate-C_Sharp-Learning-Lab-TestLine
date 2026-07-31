bccdf8dadb6e8d9a06e86e5a2b63781bf95343ba
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-31 23:15:27 +0600

    page count added in dynamically

--- EcommerceWebApi/Controllers/PaginatedRecord.cs
+++ EcommerceWebApi/Controllers/PaginatedRecord.cs
@@ -12,6 +12,6 @@ namespace EcommerceWebApi.Controllers
         public int TotalCount {get; set;}
         public int PageNumber {get; set;}
         public int PageSize {get; set;}
-        public int TotalPages {get; set;}
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
 }
