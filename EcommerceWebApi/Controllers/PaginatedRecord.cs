dd92c618e3e410fed24dc5e8de7ff0e28ce2881d
Md Al Amin <mdal.amin5@northsouth.edu>
2026-07-31 23:14:15 +0600

    total page vairable added

--- EcommerceWebApi/Controllers/PaginatedRecord.cs
+++ EcommerceWebApi/Controllers/PaginatedRecord.cs
@@ -12,5 +12,6 @@ namespace EcommerceWebApi.Controllers
         public int TotalCount {get; set;}
         public int PageNumber {get; set;}
         public int PageSize {get; set;}
+        public int TotalPages {get; set;}
     }
 }
