07ce9ca2b860fa333f99e328483f2b4eab1a5608
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-01 12:48:11 +0600

    helper class define for query parameters

--- EcommerceWebApi/Helpers/QueryParameters.cs
+++ EcommerceWebApi/Helpers/QueryParameters.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 namespace EcommerceWebApi.Helpers
 {
     public class QueryParameters
-    {
+    {   
+        private const int MaxPageSize = 50;
+        public int PageNumber {get; set;} = 1;
+        public int PageSize {get; set;} = 6;
+        public string? Search {get; set;}
+
+        public QueryParameters Validate()
+        {
+            if(PageNumber < 1)
+                PageNumber = 1;
         
+            if(PageSize < 1)
+                PageSize = 6;
+            
+            if(PageSize > MaxPageSize)
+                PageSize = MaxPageSize;
+
+            return this;
+        }
     }
+
+    
 }
