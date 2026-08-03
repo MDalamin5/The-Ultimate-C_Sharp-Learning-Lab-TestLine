3f184deff4e33da80f3059c5f9123f3088dd94cf
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-03 16:27:33 +0600

    api controller creations

---
+++ EfCorePractice/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCorePractice.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BookController: ControllerBase
+    {
+        
+    }
+}
