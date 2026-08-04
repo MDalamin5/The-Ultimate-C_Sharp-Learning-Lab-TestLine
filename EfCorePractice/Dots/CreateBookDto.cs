aa039162e5bf1766e44195f712a7f1a76b76e946
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-04 22:05:17 +0600

    Start to building the Dtos

---
+++ EfCorePractice/Dots/CreateBookDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCorePractice.Dots
+{
+    public class CreateBookDto
+    {
+        public string? Title {get; set;}
+    }
+}
