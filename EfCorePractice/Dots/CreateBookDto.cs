1f1207ea2a295dc2c7b586723c4095193b2ad820
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-04 22:07:09 +0600

    create book deto model defines

--- EfCorePractice/Dots/CreateBookDto.cs
+++ EfCorePractice/Dots/CreateBookDto.cs
@@ -8,5 +8,8 @@ namespace EfCorePractice.Dots
     public class CreateBookDto
     {
         public string? Title {get; set;}
+        public string? Description {get; set;}
+        public string? Author {get; set;}
+        public decimal Price {get; set;}
     }
 }
