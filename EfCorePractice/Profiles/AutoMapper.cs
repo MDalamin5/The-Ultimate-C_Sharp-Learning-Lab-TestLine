5b4929ac3401a440235e65624b6254af06be233d
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-06 23:47:24 +0600

    automapper added

---
+++ EfCorePractice/Profiles/AutoMapper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCorePractice.Profiles
+{
+    public class AutoMapper
+    {
+        
+    }
+}
