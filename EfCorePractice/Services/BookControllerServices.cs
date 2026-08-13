01831d696779aff4f6e94c04c6f4c575d3f4684b
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-13 23:08:26 +0600

    start to implement the Book Services class

--- EfCorePractice/Services/BookControllerServices.cs
+++ EfCorePractice/Services/BookControllerServices.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EfCorePractice.Interfaces;
 
 namespace EfCorePractice.Services
 {
-    public class BookControllerServices
+    public class BookControllerServices: IBookServices
     {
-        
+        public async Task<string> GetAllBook()
+        {
+            return "Data Data Return.";
+        }
     }
 }
