3db575385f4d02361e699ae7371ab98cca80080b
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-13 23:11:28 +0600

    book services sclatone defined

--- EfCorePractice/Services/BookControllerServices.cs
+++ EfCorePractice/Services/BookControllerServices.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using EfCorePractice.Interfaces;
@@ -8,9 +9,24 @@ namespace EfCorePractice.Services
 {
     public class BookControllerServices: IBookServices
     {
-        public async Task<string> GetAllBook()
+        public async Task<string> GetAllBooks()
         {
             return "Data Data Return.";
+        }
+
+        public async Task<string> GetASingeBook(int id)
+        {
+            return "Book ONly";
+        }
+
+        public async Task<string> UpdateBook(int Id)
+        {
+            return "Update BOOk";
+        }
+
+        public async Task<bool> DeleteBook(int Id)
+        {
+            return true;
         }
     }
 }
