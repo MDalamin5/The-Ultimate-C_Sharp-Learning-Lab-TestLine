fe90d5eee481904972f9a962b2b288aec7453123
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-16 17:29:16 +0600

    Book Model update

---
+++ EfCorePractice/Migrations/20260816112733_ChangeCreatedToDateOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCorePractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCreatedToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Created",
+                table: "Books",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Books",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
--- EfCorePractice/Models/Book.cs
+++ EfCorePractice/Models/Book.cs
@@ -11,7 +11,7 @@ namespace EfCorePractice.Models
         public string? Title {get; set;}
         public string? Description {get; set;}
         public string? Author {get; set; }
-        public DateTime Created {get; set;}
+        public DateOnly Created {get; set;}
         public decimal Price {get; set;}
     }
 }
