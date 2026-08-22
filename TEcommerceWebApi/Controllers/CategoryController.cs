4465d9f7aa56c2f828640c8693d09fa1a639366b
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-22 16:34:01 +0600

    get-categories define

--- TEcommerceWebApi/Controllers/CategoryController.cs
+++ TEcommerceWebApi/Controllers/CategoryController.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TEcommerceWebApi.Models;
 
 namespace TEcommerceWebApi.Controllers
 {
-    public class CategoryController
+    [ApiController]
+    [Route("/api/v2/categories")]
+    public class CategoryController: ControllerBase
     {
-        
+        private static List<Category> categories = new List<Category>();
+
+        public IActionResult GetCategories([FromQuery] string searchVale = "")
+        {
+            if (!string.IsNullOrEmpty(searchVale))
+            {
+                var srcCategory = categories.Where(c => c.Name.Contains(searchVale, StringComparison.OrdinalIgnoreCase)).ToList();
+                return Ok(srcCategory);
+            }
+            return Ok(categories);
+        }
     }
 }
