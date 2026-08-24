1976d37af3212de6a1138e8f4442d80144ab1674
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-24 11:51:26 +0600

    centralized api responses

---
+++ TEcommerceWebApi/Controllers/ApiResponse.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEcommerceWebApi.Controllers
+{
+    public class ApiResponse<T>
+    {
+        public bool Success {get; set;}
+        public string Message {get; set;} = string.Empty;
+        public T? Data {get; set;}
+        public List<string>? Errors {get; set;}
+        public int StatusCode {get; set;}
+        public DateTime TimeStamp {get; set;}
+
+
+        public ApiResponse(T data, int statusCode, string message = "")
+        {
+            Success = true;
+            Message = message;
+            Data = data;
+            Errors = null;
+            StatusCode = statusCode;
+            TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
