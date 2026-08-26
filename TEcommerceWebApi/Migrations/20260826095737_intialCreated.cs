bd5b3d2525dde4fd4287a01b0a105ca87cb05849
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-26 19:29:13 +0600

    first migration done

---
+++ TEcommerceWebApi/Migrations/20260826095737_intialCreated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TEcommerceWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class intialCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
