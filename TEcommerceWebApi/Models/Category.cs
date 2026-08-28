453a93f4b9687d1d62d9d468230ff4a9e22f9f1f
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-28 15:09:20 +0600

    Set the Orm connection category to product table

--- TEcommerceWebApi/Models/Category.cs
+++ TEcommerceWebApi/Models/Category.cs
@@ -11,5 +11,6 @@ namespace TEcommerceWebApi.Models
         public string Name {get; set;} = string.Empty;
         public string ?Description {get; set;}
         public DateTime CreatedAt {get; set;}
+        public ICollection<Product> Products {get; set;} = new List<Product>();
     }
 }
