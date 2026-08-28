d73adacd0fed5c064bb7025cb7ed8d0200c53413
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-28 15:15:18 +0600

    write the Fluent api code on appdebcontext

--- TEcommerceWebApi/data/AppDbContext.cs
+++ TEcommerceWebApi/data/AppDbContext.cs
@@ -12,5 +12,20 @@ namespace TEcommerceWebApi.data
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options){ }
 
         public DbSet<Category> ?Categories {get; set;}
+        public DbSet<Product> Products {get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
     }
 }
