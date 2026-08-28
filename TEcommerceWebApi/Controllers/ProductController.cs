be40fae3e48924077954cde32e8f57bbccb13c43
Md Al Amin <mdal.amin5@northsouth.edu>
2026-08-28 16:47:47 +0600

    product added using dtos

---
+++ TEcommerceWebApi/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TEcommerceWebApi.data;
+using TEcommerceWebApi.DTOs;
+using TEcommerceWebApi.Models;
+
+namespace TEcommerceWebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/v2/products")]
+    public class ProductController: ControllerBase
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        
+
+        // Create a Product
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductCreateDto productData)
+        {
+            var category = await _appDbContext.Categories.FindAsync(productData.CategoryId);
+            
+            if (category == null)
+            {
+                return NotFound(ApiResponse<object>.ErrorResponse(
+                    new List<string> { $"Category with ID '{productData.CategoryId}' does not exist." }, 
+                    404, 
+                    "Validation Failed."
+                ));
+            }
+
+            var newProduct = new Product
+            {
+                ProductId = Guid.NewGuid(),
+                Name = productData.Name,
+                Price = productData.Price,
+                CategoryId = productData.CategoryId
+            };
+
+            await _appDbContext.Products.AddAsync(newProduct);
+            await _appDbContext.SaveChangesAsync();
+
+            var responseProduct = new ProductReadDto
+            {
+                ProductId = newProduct.ProductId,
+                Name = newProduct.Name,
+                Price = newProduct.Price,
+                CategoryId = newProduct.CategoryId,
+                CategoryName = category.Name
+            };
+
+            return Ok(ApiResponse<ProductReadDto>.SuccessResponse(responseProduct, 201, "Product created successfully."));
+        }
+    }
+}
+
