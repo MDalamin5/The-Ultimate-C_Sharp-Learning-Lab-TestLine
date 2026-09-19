d42cb734a8d55b2336dbe8d8c27292cf6395701a
Md Al Amin <mdal.amin5@northsouth.edu>
2026-09-19 10:28:10 +0600

    Department repository defined

--- OrganizationManagement/IRepository/IDepartmentRepository.cs
+++ OrganizationManagement/IRepository/IDepartmentRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OrganizationManagement.DTOs.Department;
 
 namespace OrganizationManagement.IRepository
 {
     public interface IDepartmentRepository
     {
-        
+        Task createCategories(DepartmentCreateDto model);
+        Task getAllDepartments();
+        Task getDepartmentById(Guid id);
+        Task updateDepartmentById(Guid id, DepartmentUpdateDto model);
+        Task deleteDepartmentById(Guid id);
     }
 }
--- OrganizationManagement/Repository/DepartmentRepository.cs
+++ OrganizationManagement/Repository/DepartmentRepository.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OrganizationManagement.IRepository;
 
 namespace OrganizationManagement.Repository
 {
-    public class DepartmentRepository
+    public class DepartmentRepository: IDepartmentRepository
     {
         
     }
